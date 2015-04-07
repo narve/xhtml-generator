b902b4b4ad607b1927a23b065664cdce3d1e589c
Narve Sætre <narve@dv8.no>
2015-04-07 14:30:47 +0200

    wrong generics in body

--- src/main/java/no/dv8/xhtml/generation/elements/body.java
+++ src/main/java/no/dv8/xhtml/generation/elements/body.java
@@ -2,5 +2,5 @@ package no.dv8.xhtml.generation.elements;
 
 import no.dv8.xhtml.generation.support.ElementBase;
 
-public class body extends ElementBase<html> {
+public class body extends ElementBase<body> {
 }
