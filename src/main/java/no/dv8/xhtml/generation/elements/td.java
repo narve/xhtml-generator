45cf47e2cf5b678bc8f4cb2279e78229fca1e845
Narve Sætre <narve@dv8.no>
2015-04-10 18:05:49 +0200

    tables

---
+++ src/main/java/no/dv8/xhtml/generation/elements/td.java
@@ -0,0 +1,13 @@
+package no.dv8.xhtml.generation.elements;
+
+import no.dv8.xhtml.generation.support.ElementBase;
+
+public class td extends ElementBase<td> {
+    public td() {
+        super();
+    }
+    public td(String content) {
+        super();
+        add(content);
+    }
+}
---
+++ src/main/java/no/dv8/xhtml/generation/elements/th.java
@@ -0,0 +1,14 @@
+package no.dv8.xhtml.generation.elements;
+
+import no.dv8.xhtml.generation.support.ElementBase;
+
+public class th extends ElementBase<th> {
+    public th() {
+        super();
+    }
+
+    public th(String content) {
+        super();
+        add(content);
+    }
+}
